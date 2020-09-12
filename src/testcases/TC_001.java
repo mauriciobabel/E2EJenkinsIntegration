b3cb44a8d52b7ede4437f2f5af376c8c7d666431
Mauricio Orta <mauriciojose.orta@babel.es>
2020-09-12 11:45:28 +0200

    First commit

---
+++ src/testcases/TC_001.java
@@ -0,0 +1,13 @@
+package testcases;
+
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+public class TC_001 {
+	
+	@Test
+	public void testcase1() {
+		ChromeDriver driver = new ChromeDriver();
+	}
+
+}
