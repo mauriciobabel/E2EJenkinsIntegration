c3997ee603b07fecfa40bb92b064cffa4803df91
Mauricio Orta <mauriciojose.orta@babel.es>
2020-09-15 13:00:34 +0200

    Beginning first test implementation

--- src/testcases/TC_001.java
+++ src/testcases/TC_001.java
@@ -1,13 +1,23 @@
 package testcases;
 
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.AfterMethod;
 import org.testng.annotations.Test;
 
 public class TC_001 {
 	
+	private ChromeDriver driver;
+	
 	@Test
 	public void testcase1() {
+		System.setProperty("webdriver.chrome.driver", "./Driver/chromedriver.exe");
 		ChromeDriver driver = new ChromeDriver();
+		driver.get("https://www.facebook.com");
+	}
+	
+	@AfterMethod
+	public void closeDriver() {
+		driver.quit();
 	}
 
 }
